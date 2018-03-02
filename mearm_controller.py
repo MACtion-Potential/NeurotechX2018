020c033d15286ead2c4182bb4ba1c8bfdc274f98
Sam - Mini-tron <none@none>
2018-03-02 16:35:14 -0500

    Added mearm controller so we can interface with a pre-programmed arduino controlling a mearm

---
+++ mearm_controller.py
@@ -0,0 +1,35 @@
+import serial
+
+CNTR = 'M09508.004.00\n'
+TOPR = 'M05508.009.00\n'
+TOPL = 'M11508.009.00\n'
+BOTR = 'M05508.000.00\n'
+BOTL = 'M11508.000.00\n'
+CLAP = 'M09510.006.01\n'
+dance = [TOPR, CNTR, TOPR, CNTR,
+          TOPL, CNTR, TOPL, CNTR,
+          BOTR, CNTR ,BOTR, CNTR,
+          BOTL, CNTR, BOTL, CNTR,
+          TOPR, CNTR, TOPL, CNTR,
+          BOTR, CNTR, BOTL, CNTR,
+          CLAP, CNTR, CLAP, CNTR]
+
+class MeArm:
+  port = '';
+  serial = None;
+  __halt__ = False;
+  messages = dance;
+  def __init__(self, port, baud):
+    self.port = port;
+    self.serial = serial.Serial(port, baud);
+  def halt(self):
+    self.__halt__ = True;
+  def resume(self):
+    self.__halt__ = False;
+  def write(self):
+    if self.__halt__: return;
+    self.serial.write(self.messages[0])
+    self.messages.append(self.messages[0])
+    self.messages = self.messages[1:]
+  def close(self):
+    self.serial.close();
